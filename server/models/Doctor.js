bcb62c055639eb71f7a08cdda5a6b6607487cc7a
Joyel15 <joyeldsza@gmail.com>
2026-07-05 14:16:27 +0530

    feat: add Doctor model

---
+++ server/models/Doctor.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+// Schema to store additional information for users with the "doctor" role
+const doctorSchema = new mongoose.Schema(
+  {
+    // Reference to the corresponding user account
+    doctorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    specialization: {
+      type: String,
+      required: true,
+    },
+    experience: {
+      type: Number,
+      required: true,
+    },
+    fees: {
+      type: Number,
+      required: true,
+    },
+    availability: [
+      {
+        day: {
+          type: String,
+          required: true,
+        },
+        startTime: {
+          type: String,
+          required: true,
+        },
+        endTime: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    isApproved: {
+      type: Boolean,
+      default: false,
+    },
+    bio: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Doctor = mongoose.model("Doctor", doctorSchema);
+
+export default Doctor;
